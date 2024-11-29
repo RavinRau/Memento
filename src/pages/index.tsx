88f3955d6e452d0ec4ab49d58bf9a9ecda794354
Ravin Rau <ravinrau1@gmail.com>
2024-11-30 04:58:59 +0800

    remove the unwanted imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,3 @@
-import { Button } from "@/components/Button/Button";
-import Card from "@/components/Card/Card";
-import Dropdown from "@/components/Dropdown/Dropdown";
-import { DropdownItemProps } from "@/components/Dropdown/DropdownTypes";
-import { User, CreditCard, Settings, Keyboard, UserPlus, Mail, MessageSquare, MoreHorizontalIcon } from "lucide-react";
-import { toast } from "sonner";
-
 export default function Home() {
 
   return (
